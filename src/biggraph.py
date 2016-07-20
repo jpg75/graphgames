a6e29325eec913e0158b82c6b21701502c4a5eea
Gian Paolo Jesi <gp.jesi@gmail.com>
2016-07-20 17:24:00 +0200

    + added graph_gen() function to generate the random block-type graphs

--- src/biggraph.py
+++ src/biggraph.py
@@ -1,16 +1,32 @@
 __author__ = 'Gian Paolo Jesi'
 
-from numpy.random import random
 from util.algo import split_check, spanning
 from graph_tool.generation import geometric_graph, random_graph
+from numpy import random
 from graph_tool.draw import graph_draw, sfdp_layout
 from graph_tool import load_graph
 from timeit import default_timer as timer
 
-if __name__ == '__main__':
+
+def _corr(a, b):
+    if a == b:
+        return 0.999
+    else:
+        return 0.001
+
+
+def graph_gen():
     # points = random((10000, 2)) * 4
     # g, pos = geometric_graph(points, 0.3, [(0, 4), (0, 4)])
-    g = load_graph('data/bg2000.xml.gz')
+    g, bm = random_graph(20000, lambda: random.poisson(10), directed=False,
+                         model="blockmodel-traditional",
+                         block_membership=lambda: random.randint(10),
+                         vertex_corr=_corr)
+    return g, bm
+
+
+if __name__ == '__main__':
+    g = load_graph('data/bg20000.xml.gz')
     print "Graph loaded: ", g
 
     print "Calculating distance matrix..."
