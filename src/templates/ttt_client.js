ec5b618936daf186e5cc8571a851a939a0083ff3
Gian Paolo Jesi <gp.jesi@gmail.com>
2016-09-14 17:59:44 +0200

    + ttt_client.js implements all TTT game logic for playing a hand + animation engine works

--- src/templates/ttt_client.js
+++ src/templates/ttt_client.js
@@ -1,101 +1,171 @@
 const cards_colors = {"2H": "red", "3H": "red", "4H": "red",
                     "2C": "black", "3C": "black", "4C": "black"};
-let player = 'CK';
+let player = 'CK', score = 0, gameCard = '2H';
+const allowed_moving_zones = ["U", "T", "C", "N"];
 
 $(document).ready(function () {
-/* initial card positions:
-NK    N   UP   C    CK   T	GC	PL
-3C   4H   2H   3H   2C   4C	2H	ck
-*/
-      window.startPos = window.endPos = {};
+	/* initial card positions:
+	NK    N   UP   C    CK   T	GC	PL
+	3C   4H   2H   3H   2C   4C	2H	ck
+	*/
+	window.startPos = window.endPos = {};
 	  
-      init();
-      makeDraggable();
+	initCardsData();
+	makeDraggable();
 
-      $('.cardSlot').droppable({
-        hoverClass: 'hoverClass',
-        over: function( event, ui ) {
-        	over_what = $(ui.draggable);
-        	},
-
+    $('.cardSlot').droppable({
+    	hoverClass: 'hoverClass',
+        
         drop: function(event, ui) {
-          var $from = $(ui.draggable),
-              $fromParent = $from.parent(),
-              $to = $(this).children(),
-              $toParent = $(this);
+        	let $from = $(ui.draggable),
+            	$fromParent = $from.parent(),
+            	$to = $(this).children(),
+              	$toParent = $(this);
 
-			// Invert the players when an exchange is legal
-			if (! $toParent.is($fromParent)) {
-		  		if (player=='CK') {
-		  			player = 'NK';
-          		}
-          		else {
-          			player = 'CK';
-          		}
-          		console.log("Are the same!");
-		  	}
-	
-		  console.log('From: ', $from);
-		  console.log('To: ', $to);
+			legal_move = checkMove($from, $fromParent, $to, $toParent);
+			console.log("Is legal: "+ legal_move);
+			
+			if (!legal_move) {
+				// restore the moved card to its previous position:
+				window.endPos = $from.offset();
+				swap($from, window.endPos, window.startPos, 500, function() {
+            		$toParent.html($to.css({position: 'relative', left: '', top: '', 'z-index': ''}));
+            		$fromParent.html($from.css({position: 'relative', left: '', top: '', 'z-index': ''}));
+            		makeDraggable();
+          		});
+			}
+			else {			
+				// Invert the players when an exchange is legal: 
+				// the parent objects are different
+				if (! $toParent.is($fromParent) && legal_move) {
+					invertPlayers($fromParent);
+		  		}
 
-          window.endPos = $to.offset();
+				window.endPos = $to.offset();
 
-          swap($from, $from.offset(), window.endPos, 200);
-          swap($to, window.endPos, window.startPos, 500, function() {
-            $toParent.html($from.css({position: 'relative', left: '', top: '', 'z-index': ''}));
-            $fromParent.html($to.css({position: 'relative', left: '', top: '', 'z-index': ''}));
-            makeDraggable();
-          });
+				swap($from, $from.offset(), window.endPos, 200);
+    	      	swap($to, window.endPos, window.startPos, 500, function() {
+        	    	$toParent.html($from.css({position: 'relative', left: '', top: '', 'z-index': ''}));
+            		$fromParent.html($to.css({position: 'relative', left: '', top: '', 'z-index': ''}));
+            		makeDraggable();
+          		});
 
+          		score++;
+          	}
 	    }
-      });
+	});
+	  
+	function swap($el, fromPos, toPos, duration, callback) {
+		$el.css('position', 'absolute')
+        	.css(fromPos)
+        	.animate(toPos, duration, function() {
+            	if (callback) callback();
+          	});
+	}
+});
 
-      function makeDraggable() {
-		/* let checkedValue = $('.limitDrag').is(":checked");*/
-		let checkedValue = document.getElementById("limitDrag").checked;
-		console.log(checkedValue); 
-
-		/* $('.card').draggable({ */
-        $('.card').draggable({
-          disabled: true,
-          containment: '#content',
-          zIndex: 99999,
-          revert: 'invalid',
-          start: function(event, ui) {
-            window.startPos = $(this).offset();
-          }
-        });
-
-		/* Enable the draggable(s) corresponding to the NK or CK cards*/
-		$('#'+player+ '> .card').draggable('enable');
-		
-      }
-
-      function swap($el, fromPos, toPos, duration, callback) {
-        $el.css('position', 'absolute')
-          .css(fromPos)
-          .animate(toPos, duration, function() {
-            if (callback) callback();
-          });
-      }
-    });
-
-function init(){
-    let i = 0;
-	
+/**
+* Generate a sort of metadata inside the card slots
+*/ 
+function initCardsData(){
 	$('.card').each(function(index, el) {
-		$( this ).data('color', 'red');
+		let key = $(this).text();
+		console.log('key: '+key);
+		$(this).data('color', cards_colors[key] );
+		// $( this ).data('color', 'red');
 		console.log( index + ": " + $( this ).data('color') );
 		$(this).data('number', $(this).text().charAt(0) );
 		console.log( index + ": " + $( this ).data('number') );
-		
 		console.log( index + ": " + $( this ).text() );
-	})
+	});
 }
 
-/* Called when the PASS button is pressed. */
+/**
+* Make every card draggable, but disabled. Only the card's current player is made 
+enabled and emphasized.
+*/
+function makeDraggable() {
+	let checkedValue = document.getElementById("limitDrag").checked;
+	console.log(checkedValue); 
+
+    $('.card').draggable({
+    	disabled: true,
+    	containment: '#content',
+    	zIndex: 99999,
+    	revert: 'invalid',
+    	start: function(event, ui) {
+    		window.startPos = $(this).offset();
+    	}
+    });
+
+	/* Enable the draggable(s) corresponding to the NK or CK cards*/
+	$('#'+player+ '> .card').draggable('enable');
+	emphasizeActivePlayer();
+	initCardsData();
+}
+
+/**
+* Check if a move is legal or not.
+*/
+function checkMove($from, $fromP, $to, $toP){
+	let dest_slot = $toP.attr('id');
+	console.log('id of destination: '+dest_slot);
+
+	let index = allowed_moving_zones.lastIndexOf(dest_slot);
+	if (index == -1) return false;
+
+	if (dest_slot == 'T'){
+		let $tcard = $('#T > .card');
+		let color_t = $tcard.data('color');
+		let number_t = $tcard.data('number');
+
+		console.log('player: '+player+' , pcolor: '+$from.data('color')+ ' - Tcolor: '+color_t+
+			' , pnumber: '+  $from.data('number')+ ' - Tnumber: '+number_t );
+
+		if (player == 'CK' && $from.data('color') == color_t) return true;
+		else if (player == 'NK' && $from.data('number') == number_t) return true;
+		else return false;
+	}
+	else return true;
+}
+
+/**
+* Make the current player card slot thicker and colored (red). 
+*/
+function emphasizeActivePlayer() {
+	$('#'+player).css('border', '2px solid red');
+}
+
+/**
+* Invert the players and reset the previous player card slot with its basic
+* color.
+*/
+function invertPlayers($fromParent) {
+	// Invert the players and sets a red border over the current one
+	if (player=='CK') {
+		player = 'NK';
+		$fromParent.css('border', '2px solid #333');
+	}
+	else {
+		player = 'CK';
+		$fromParent.css('border', '2px solid #333');
+	}
+}
+
+/** 
+* Called when the PASS button is pressed. Invert the players and regenerate the 
+* card draggables.
+*/
 function passMove(){
 	let output_n = $('.card').data('number');
 	console.log("Data: ", output_n);
+
+	invertPlayers($('#'+player));
+	makeDraggable();
 }
 
+// graph decompositions, Diestel
+// Rasetti
+// Knoblock
+// abstracting the tower of hanoi
+// richard korf, rubik ; 1985
