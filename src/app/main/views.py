e6092ed47e877fb01cc9ff0aaa70e4b9be308360
Gian Paolo Jesi <gp.jesi@gmail.com>
2016-11-15 10:26:59 +0100

    + in main/view.py:  added show_game()  as variable route for games + index.html generate a table with all games availbale in SessionType db class; id and info of each item are stored in a dictionary passed to the html template

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -1,16 +1,20 @@
+from OpenSSL.SSL import Session
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
 from .forms import NameForm
 from .. import db
-from ..models import User
+from ..models import User, SessionType
 from ..decorators import authenticated_only
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    avail_games = ['ttt']
+    # avail_games = ['ttt']
+    avail_games = SessionType.query.all()
+    avail_games = {x.id: x.info for x in avail_games}
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
@@ -34,4 +38,10 @@ def index():
 @main.route('/ttt')
 @authenticated_only
 def ttt():
+    return render_template("ttt-page.html")
+
+
+@main.route('/<int:game_id>')
+@authenticated_only
+def show_game(game_id):
     return render_template("ttt-page.html")
