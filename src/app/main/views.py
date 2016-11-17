59c51bfd381d3ec7ca2e663facea49b84026c0cc
Gian Paolo Jesi <gp.jesi@gmail.com>
2016-11-17 18:34:42 +0100

    + updated and generated routes for all html files

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -1,11 +1,11 @@
 # from OpenSSL.SSL import Session
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, flash
+from flask import render_template, session, redirect, url_for, flash, request
 from flask_login import current_user, login_required
 from . import main
-from .forms import NameForm, BaseForm
+from .forms import NameForm, BaseForm, GameTypeForm
 from .. import db
-from ..models import User, GameType, GameSession, Role
+from ..models import User, GameType, GameSession, Role, Move
 from ..decorators import authenticated_only, admin_required
 from wtforms import SelectField, SubmitField
 from json import loads
@@ -31,15 +31,45 @@ def index():
         return redirect(url_for('.index'))
 
     return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False),
+                           form=form,
                            games=avail_games,
                            current_time=datetime.utcnow())
+
+
+@main.route('/gsessions/<username>')
+@login_required
+def sessions(username):
+    gs = GameSession.query.filter_by(uid=current_user.id).all()
+
+    return render_template('sessions.html', user=current_user, data=gs)
+
+
+@main.route('/gsessions/<int:sid>')
+@login_required
+def session_moves(sid):
+    s = Move.query.filter_by(sid=sid).all()
+
+    return render_template('session.html', user=current_user, data=s, sid=sid)
 
 
 @main.route('/admin', methods=['GET', 'POST'])
 @login_required
 def admin():
+    if not current_user.is_administrator():
+        flash('Cannot access to admin panel when not having administrative privileges.')
+        redirect(url_for('.index'))
+
+    else:
+        return render_template('admin.html')
+
+
+@main.route('/user_admin')
+@login_required
+def user_admin():
+    if not current_user.is_administrator():
+        flash('Cannot access to admin panel features when not having administrative privileges.')
+        redirect(url_for('.index'))
+
     users = User.query.all()
     for user in users:
         print user.username
@@ -52,30 +82,81 @@ def admin():
     BaseForm.append_field('Submit privilege change', SubmitField())
     form = BaseForm()
 
-    if not current_user.is_administrator():
-        flash('Cannot access to admin panel when not having administrative privileges.')
-        redirect(url_for('.index'))
-
     if form.validate_on_submit():
         print form.data
         for user in form.data:
             if user.startswith('user_'):
                 uid = int(user.replace('user_', ''))
-                users[uid-1].role_id = Role.role_id_from_name(form.data[user])
-                db.session.add(users[uid-1])
+                users[uid - 1].role_id = Role.role_id_from_name(form.data[user])
+                db.session.add(users[uid - 1])
 
         db.session.commit()
         flash('Privileges updated.')
         return redirect(url_for('.index'))
-    
-    else:
-        return render_template('admin.html', form=form)
+
+    return render_template('user_admin.html', form=form)
 
 
-@main.route('/ttt')
-@authenticated_only
-def ttt():
-    return render_template("ttt-page.html")
+@main.route('/config')
+@login_required
+def config():
+    if not current_user.is_administrator():
+        flash('Cannot access to admin panel features when not having administrative privileges.')
+        redirect(url_for('.index'))
+
+    st = GameType.query.all()
+
+    return render_template('config.html', data=st)
+
+
+@main.route('/config_add', methods=['GET', 'POST'])
+@login_required
+def config_add():
+    if not current_user.is_administrator():
+        flash('Cannot access to admin panel features when not having administrative privileges.')
+        redirect(url_for('.index'))
+
+    form = GameTypeForm()
+    if form.validate_on_submit():
+        gt = GameType(info=form.info.data, params=form.params.data)
+        db.session.add(gt)
+        db.session.commit()
+        flash('New game configuration injected.')
+        return redirect(request.args.get('next') or url_for('main.config'))
+
+    return render_template('config_add.html', form=form)
+
+
+@main.route('/config_edit/<int:conf_id>', methods=['GET', 'POST'])
+@login_required
+def config_edit(conf_id):
+    if not current_user.is_administrator():
+        flash('Cannot access to admin panel features when not having administrative privileges.')
+        redirect(url_for('.index'))
+
+    gt = GameType.query.filter_by(id=conf_id).first()
+    form = GameTypeForm()
+
+    if form.validate_on_submit():
+        gt.info = form.info.data
+        gt.params = form.params.data
+        db.session.add(gt)
+        db.session.commit()
+        flash('Configuration updated.')
+        return redirect(request.args.get('next') or url_for('main.config'))
+
+    form.info.data = gt.info
+    form.params.data = gt.params
+    form.submit.label.text = 'Update configuration'
+
+    return render_template('config_edit.html', form=form, conf_id=conf_id)
+
+
+@main.route('/config_del/<int:conf_id>')
+@login_required
+def config_del(conf_id):
+    form = BaseForm()
+    return render_template('config_del.html', form=form, conf_id=conf_id)
 
 
 @main.route('/<int:game_id>')
@@ -100,13 +181,14 @@ def show_game(game_id):
     s = GameSession(uid=current_user.id, type=game_id, start=datetime.now())
     db.session.add(s)
     db.session.commit()
+
     session['game_session'] = s.id
 
     st = GameType.query.filter_by(id=game_id).first()
     session['game_cfg'] = loads(st.params)  # the json from the DB is converted into python dict
     session['game_type'] = game_id
 
-    # return render_template(session['game_cfg']['html-file'])
+    # return render_template(session['game_cfg']['html_file'])
     return render_template("ttt-page.html")
 
     # NOTE:
