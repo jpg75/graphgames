f3061ae01d26a2e38b22649d64e16fdf9219c800
Gian Paolo Jesi <gp.jesi@gmail.com>
2016-11-17 18:33:50 +0100

    + added GameTypeForm

--- src/app/auth/forms.py
+++ src/app/auth/forms.py
@@ -15,10 +15,6 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
-    # username = StringField('Username', validators=[
-    #     DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-    #                                           'Usernames must have only letters, '
-    #                                           'numbers, dots or underscores')])
     password = PasswordField('Password', validators=[DataRequired(), EqualTo(
         'password2',
         message='Password '
@@ -27,9 +23,11 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('Email already registered.')
+
 
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
--- src/app/main/forms.py
+++ src/app/main/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import data_required
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField, ValidationError
+from wtforms.validators import Email, Length, EqualTo, DataRequired, Regexp
+from json import loads
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[data_required()])
-    password = PasswordField('Password', validators=[data_required()])
+    name = StringField('What is your name?', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -15,3 +16,17 @@ class BaseForm(FlaskForm):
         setattr(cls, name, field)
         return cls
 
+
+class GameTypeForm(FlaskForm):
+    info = StringField('Info about the game configuration', validators=[DataRequired()])
+    params = TextAreaField('Configuration parameters (in JSON string format)', validators=[
+        DataRequired()])
+
+    submit = SubmitField('Add Configuration')
+
+
+    def validate_params(self, field):
+        try:
+            loads(field.data)
+        except ValueError as ve:
+            raise ValidationError('No valid JSON string in parameters.')
