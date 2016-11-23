280d1a68980d657166de55f9f83979efffcf699a
Gian Paolo Jesi <gp.jesi@gmail.com>
2016-11-23 13:08:20 +0100

    + view.py disabled method before_request() to quickly address a bug in admin authentication: TO BE RESTORED LATER

--- src/app/auth/views.py
+++ src/app/auth/views.py
@@ -74,10 +74,10 @@ def resend_confirmation():
 
 
 # Required in order to not allow unconfirmed users the access
-@auth.before_app_request
-def before_request():
-    if current_user.is_authenticated():
-        current_user.ping()
+#@auth.before_app_request
+#def before_request():
+#    if current_user is not None:
+#        current_user.ping()
         # if not current_user.confirmed and request.endpoint[:5] != 'auth.':
         #     return redirect(url_for('auth.unconfirmed'))
 
