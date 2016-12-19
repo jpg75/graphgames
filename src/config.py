95d626bdf4df91e515e3a97d44c232db6540ae6e
Gian Paolo Jesi <gp.jesi@gmail.com>
2016-12-19 16:19:45 +0100

    + added parameter keys for Celery integration

--- src/config.py
+++ src/config.py
@@ -11,6 +11,9 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SOCKET_IO_PORT = 5000
+    CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL') or 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND') or \
+                            'redis://localhost:6379/0'
     SHOE_FILE_ORDER = ['NK', 'N', 'U', 'C', 'CK', 'T', 'GC', 'PL']
 
     @staticmethod
