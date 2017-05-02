b454dd0ac82e2a1e3e5b7e97fe83da2e757d045f
Gian Paolo Jesi <gp.jesi@gmail.com>
2017-05-02 10:55:38 +0200

    + added requirements.txt for virtualenv regeneration; runme.sh uses the venv and generate it when not present

--- src/runme.sh
+++ src/runme.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+# python ./manage.py run
+
+if [ ! -d "flaskenv" ]; then
+    echo "Python virtual environment for web app not present. Installing now..."
+    virtualenv flaskenv
+    source flaskenv/bin/activate
+    pip install -r requirements.txt
+    deactivate
+
+fi
+
+source flaskenv/bin/activate
 python ./manage.py run
